ec9c27df2f6c8f7ead6fc57e57e5c0f1ee2cb14c
Joako Sueyro <100146550+JoakoS03@users.noreply.github.com>
2024-04-05 18:33:06 -0300

    Update ejercicio1.go

--- ejercicio1.go
+++ ejercicio1.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	frase := "Los jueves juego al futbol"
+	frase := "Los jueves juego al futbol y los JUEVES al tenis"
 
 	frase2 := replace(frase, "jueves")
 
@@ -23,14 +23,14 @@ func replace(frase, jueves string) string {
 	var index = 0
 
 	fraseNueva := ""
-
-	for i := 0; i < len(frase2); {
+	i := 0
+	for i < len(frase2) {
 		index = strings.Index(frase2[i:], jueves)
 		if index != -1 {
-			palabra = frase2[index : index+6]
+			palabra = frase2[i+index : i+index+6]
 			if palabra == jueves {
 				var martes string
-				for j, letra := range frase[index : index+6] {
+				for j, letra := range frase[i+index : i+index+6] {
 
 					if unicode.IsUpper(rune(letra)) {
 						martes += martesMayus[j]
@@ -38,14 +38,15 @@ func replace(frase, jueves string) string {
 						martes += martesMin[j]
 					}
 				}
-				fraseNueva += frase[i:index]
+				fraseNueva += frase[i : i+index]
 				fraseNueva += martes
-				i = index + 6
+				i = i + index + 6
 			}
 		} else {
 			fraseNueva += frase[i:]
 			break
 		}
+		fmt.Println(i)
 	}
 
 	return fraseNueva
