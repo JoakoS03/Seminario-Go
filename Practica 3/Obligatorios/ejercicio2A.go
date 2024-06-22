8dd9423ae610fdef0712b049e7a1de67352eeb0d
Joako Sueyro <100146550+JoakoS03@users.noreply.github.com>
2024-06-22 16:54:50 -0300

    Update and rename ejercicio2.go to ejercicio2A.go

---
+++ Practica 3/Obligatorios/ejercicio2A.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func cargarCliente(in chan string, wg *sync.WaitGroup) {
+
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	for i := 1; i < 10; i++ {
+
+		b := make([]rune, 7)
+
+		for i := range b {
+			b[i] = letterRunes[rand.Intn(len(letterRunes))]
+		}
+		in <- string(b)
+	}
+	wg.Done()
+}
+
+func simularAtencion(caja chan string, clientes chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		cli, ok := <-clientes
+		if !ok {
+			break // El canal se cerró, salir del bucle
+		}
+
+		// Crear un temporizador para cada cliente
+		duration := time.Duration(rand.Intn(1000)) * time.Millisecond
+		timer := time.NewTimer(duration)
+
+		// Simular atención al cliente
+		<-timer.C // Esperar hasta que el temporizador expire
+		caja <- cli
+		fmt.Println("Cliente:", cli, " siendo atendido")
+		fmt.Println("El cliente:", <-caja, " fue atendido")
+	}
+	close(caja)
+
+}
+
+func main() {
+	// Channels
+	colaClientes := make(chan string)
+	caja := make(chan string, 1)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	// Agregar clientes
+	go cargarCliente(colaClientes, &wg)
+
+	// Simular atención
+	go simularAtencion(caja, colaClientes, &wg)
+
+	wg.Wait()
+
+	// Esperar a que simularAtencion termine de procesar todos los clientes
+	close(colaClientes)
+
+	fmt.Println("Finaliza la atención")
+}
